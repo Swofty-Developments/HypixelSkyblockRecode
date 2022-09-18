1cd2c9b6de82bbf4a3dac340894ba0ee4b940ba4
shadowlol2 <82683930+shadowlol2@users.noreply.github.com>
2022-09-18 14:53:07 +0200

    Update MongoCoins.java

--- SkyblockSandboxBase/src/main/java/net/atlas/SkyblockSandbox/database/mongo/MongoCoins.java
+++ SkyblockSandboxBase/src/main/java/net/atlas/SkyblockSandbox/database/mongo/MongoCoins.java
@@ -40,7 +40,7 @@ public class MongoCoins implements MongoDB {
         if (dbinfo.getConfiguration().getBoolean("use"))
             client = MongoClients.create(dbinfo.getConfiguration().getString("uri"));
         else
-            client = MongoClients.create("mongodb+srv://atlasDevelopment:qvm347n89fugyq89@cluster0.ha64p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+      /*add your database uri*/      client = MongoClients.create("mongodb+srv://atlasDevelopment:qvm347n89fugyq89@cluster0.ha64p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 
         MongoDatabase database = client.getDatabase("AtlasSandbox");
         col = database.getCollection("playerdata");
